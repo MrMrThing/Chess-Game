0783a8d0ed36bdad7fd7a365c5f1bee92f219090
Monteyi <79196763+Monteyi@users.noreply.github.com>
2021-12-09 10:27:48 +0100

    Countdown test works now

---
+++ src/Test/company/CountdownTest.java
@@ -0,0 +1,16 @@
+package company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountdownTest {
+    // find out if the milliseconds is eqeal the countdowns elapsedTime.
+    @Test
+    void CountdownTest() {
+        Countdown testCoundtown=new Countdown();
+        assertEquals(890000, testCoundtown.elapsedTime);
+
+    }
+
+}
